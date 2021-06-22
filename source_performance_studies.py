d65d4ed3c39583a1200716abd1dee45ef5d653dc
anagtv <ana.garcia-tabares.valdivieso@cern.ch>
2021-06-22 14:50:53 +0200

    Simplified version, but incomplete of the source studies

---
+++ source_performance_studies.py
@@ -0,0 +1,141 @@
+import pandas as pd
+import numpy as np
+import os
+from datetime import datetime
+import tfs
+from scipy.optimize import curve_fit
+import saving_files_summary_list_20200420
+import computing_charge_df
+
+class cumulative_charge:
+    def __init__(self):
+        self.target_current = 0
+        self.foil_individual_1 = 0
+        self.foil_individual_2 = 0
+        self.foil_individual_3 = 0
+        self.foil_individual_4 = 0
+        self.foil_individual_5 = 0
+        self.foil_individual_6 = 0
+    def cumluative_charge_calculation(self,previous_value,generic):
+        print ("HEREEEE")
+        generic = previous_value
+
+class file_information:
+    def __init__(self):
+       self.file_number = []
+       self.week = []
+       self.date_stamp = []
+    def selecting_file_summary(self,file):
+        [target_number,date_stamp,name,file_number] = saving_files_summary_list_20200420.get_headers(file)
+        self.file_number.append(file_number)
+        date_stamp_format = datetime.strptime(date_stamp, '%Y-%m-%d')
+        self.week.append(date_stamp_format.isocalendar()[1])
+        self.date_stamp.append(date_stamp)
+
+class central_region:
+    def __init__(self):
+       self.gas_flow = []
+       self.vacuum = []  
+       self.vacuum_ave = []
+       self.vacuum_std = [] 
+       self.vacuum_probe = []
+       self.probe_current_transmission = []
+       self.probe_current_transmission_std = []
+       self.probe_vacuum_transmission = []
+       self.source_current_estable = []
+       self.source_current = []
+       self.transmission_option_a = []
+       self.transmission_option_b = []
+       self.transmission_option_c = []
+    def selecting_central_region_data(self,file_df,max_current,max_current_isochronism):
+       vacuum = file_df.Vacuum_P[file_df.Target_I.astype(float) > 0.7*(max_current)].astype(float)
+       probe_current = file_df.Probe_I.astype(float)[(file_df.Probe_I.astype(float) > 14) & (file_df.Probe_I.astype(float) < 16)]
+       self.source_current_estable.append(np.average(file_df.Arc_I[file_df.Target_I.astype(float) > 0.7*(max_current)].astype(float))) 
+       self.source_current.append(file_df.Arc_I.astype(float).sum())      
+       self.probe_current_transmission.append(np.average(probe_current))
+       self.probe_current_transmission_std.append(np.std(probe_current))
+       self.probe_vacuum_transmission.append(np.average((file_df.Vacuum_P.astype(float)[(file_df.Probe_I.astype(float) > 14) & (file_df.Probe_I.astype(float) < 16)])*1e5))
+       self.transmission_option_a.append(max_current_isochronism/np.average(probe_current))
+       self.transmission_option_b.append(max_current_isochronism/np.max(probe_current))
+       self.transmission_option_c.append(np.average(max_current_isochronism/(probe_current)))
+       self.vacuum_ave.append(np.average(vacuum.astype(float)*1e5))
+       self.vacuum_std.append(np.std(vacuum.astype(float)*1e5))
+       self.gas_flow.append(np.max(file_df.Gas_flow[file_df.Target_I.astype(float) > 0.7*(max_current)].astype(float)))
+
+class rf_values: 
+    def __init__(self):
+        self.rf = []
+        self.rf_power = []
+    def selecting_central_region_data(self,file_df):
+        self.rf.append(np.max(file_df.Dee_1_kV[file_df.Target_I.astype(float) > 0.7*(max_current)].astype(float)))
+        self.rf_power.append(np.max(file_df.RF_fwd_W[file_df.Target_I.astype(float) > 0.7*(max_current)].astype(float)))
+      
+  
+class target_parameters:
+    def __init__(self):        
+        self.vacuum = []
+        self.target_number_list = []
+        self.target_current = []
+        self.foil_current = []
+        self.target_volume = []
+        self.pressure_initial_all = []
+        self.pressure_final_all = []
+        self.probe_current_estimated = []
+        self.target_to_foil_ratio = []
+    def selecting_data(self,file_df,max_current,transmission):
+        target_current = file_df.Target_I[file_df.Target_I.astype(float) > 0.7*(max_current)].astype(float)
+        foil_current = file_df.Foil_I[file_df.Target_I.astype(float) > 0.7*(max_current)].astype(float)
+        self.target_current.append(np.average(target_current))
+        self.foil_current.append(np.average(foil_current))
+        self.target_to_foil_ratio.append(np.average(target_current/foil_current))
+        self.probe_current_estimated.append(np.average(foil_current/transmission))
+        pressure_initial = np.min(file_df.Target_P.astype(float)[0:np.min(file_df.Target_P[file_df.Target_P.astype(float) > 400].index)])
+        pressure_final = file_df.Target_P.astype(float)[np.min(file_df.Target_P[file_df.Target_P.astype(float) > 400].index)]
+        self.pressure_initial_all.append(pressure_initial)
+        self.pressure_final_all.append(pressure_final)
+        self.target_volume.append((pressure_final-pressure_initial)/pressure_final)
+
+
+
+def main():
+    #folder = "/Users/anagtv/Documents/OneDrive/046 - Medical Devices/Mantenimientos ciclotrones/MRS/LOGS/2018"
+    folder = "/Users/anagtv/Documents/OneDrive/046 - Medical Devices/Mantenimientos ciclotrones/TCP/LOGS/2021"
+    filename_completed = []
+    for file in os.listdir(folder):
+         filename_completed.append(os.path.join(folder,file))
+    ranges_current = [[24.0,26.0],[49.0,52.0],[79.0,82.0]]
+    #ranges_current = [[80.0,100.0]]
+    df_total_information = pd.DataFrame(columns=["SOURCE","FOIL","TARGET","PROBE","DATE","TARGET_FOIL","TRANSMISSION","VACUUM"])
+    target = target_parameters()
+    central_region_summary = central_region()
+    file_summary = file_information()
+    week_list = []
+    filename_completed_current = []
+    transmission_all = []
+    vacuum_transmission = []
+    probe_current_all = []
+    probe_current_std_all = []
+    j = -1
+    for file in filename_completed:
+        real_values = saving_files_summary_list_20200420.get_irradiation_information(file)
+        data_df = saving_files_summary_list_20200420.get_data(real_values)
+        target_max_current = np.max(data_df.Target_I.astype(float))
+        target_average_current = np.average(data_df.Target_I.astype(float))
+        if target_average_current > 10.0:
+            j += 1
+            file_summary.selecting_file_summary(file)
+            df_isochronism = saving_files_summary_list_20200420.get_isochronism(data_df)
+            central_region_summary.selecting_central_region_data(data_df,target_max_current,np.max(df_isochronism.Foil_I))
+            target.selecting_data(data_df,target_max_current,central_region_summary.transmission_option_a[j])
+    df_source = pd.DataFrame(list(zip(file_summary.date_stamp,file_summary.file_number,central_region_summary.source_current_estable,central_region_summary.source_current,target.probe_current_estimated,target.target_current)),columns=["DATE","FILE","SOURCE_CURRENT_AVE","SOURCE_CURRENT_CUMULATIVE","PROBE","TARGET"])
+    df_source_sort = df_source.sort_values(by="FILE",ignore_index=True)
+    cummulative_source = []
+    for i in range(len(df_source_sort.PROBE)):
+        cummulative_source.append(df_source_sort.SOURCE_CURRENT_CUMULATIVE[0:i].sum()*3/3600)
+    df_source_sort["CUMULATIVE_SOURCE_CURRENT"] = cummulative_source
+    print (df_source_sort)
+       
+    
+
+if __name__ == "__main__":
+    main()
